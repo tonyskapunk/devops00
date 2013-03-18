13cfe657b0dc40b4976918b335886c33701e78ce
tonyskapunk <tonysk8@gmx.net>
2013-03-18 00:45:54 +0000

    challenge1

---
+++ challenge1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+import pyrax
+import time
+
+count = 3
+delay = 2
+distro = "arch"
+name = "xweb"
+servers = []
+size = 512
+
+creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
+pyrax.set_credential_file(creds_file)
+cs = pyrax.cloudservers
+
+imgs = cs.images.list()
+flvs = cs.flavors.list()
+
+image = [img for img in imgs if distro in img.name.lower()][0]
+flavor = [flv for flv in flvs if flv.ram == size][0]
+
+print "Requesting  %s servers with prename: %s" % (count, name)
+for index in range(count):
+  server = {}
+  s = cs.servers.create("%s%s" % (name, index), image, flavor)
+  server[s.id] = s.adminPass
+  servers.append(server)
+print "Building Servers. Might take about  %s secs..." % (20*count)
+time.sleep(20*count)
+while True:
+  if not servers:
+    break
+  server = servers[0]
+  del servers[0]
+  s = cs.servers.get(server.keys()[0])
+  if s.networks:
+    public = s.networks['public']
+    if ":" in public[0]:
+      publicv4 = public[1]
+    else:
+      publicv4 = public[0] 
+    print "%s: %s / %s" % (s.name, publicv4, server[server.keys()[0]])
+  else:
+    servers.append(server)
+  time.sleep(delay)
