2d3f37f4123b8f04580f1577d00b06152381f4dc
tonyskapunk <tonysk8@gmx.net>
2013-03-28 17:19:35 +0000

    Add support to node/lb name, interna/external port and algorithm.

--- challenge7.py
+++ challenge7.py
@@ -51,16 +51,17 @@ def isValidProtocol(protocol):
                return True
      return False
 
-def createLB(lbname, port, proto, nodes):
+def createLB(lbname, algorithm, port, proto, nodes):
      vip = createVIP()
-     lb = clb.create(lbname, port=port, protocol=proto.upper(), nodes=nodes, virtual_ips=[vip])
+     lb = clb.create(lbname, algorithm=algorithm.upper(), port=port,
+                     protocol=proto.upper(), nodes=nodes, virtual_ips=[vip])
      return lb
 
 def createNodes(srvs, port):
      nodes = []
      for srv in srvs:
           private = srv.networks['private']
-          if ":" in private[0]:
+         if ":" in private[0]:
                priv4 = private[1]
           else:
                priv4 = private[0]
@@ -88,7 +89,6 @@ def createServers(count, name, image, flavor):
      for index in range(count):
           tmp_srv = cs.servers.create("%s%s" % (name, index), image, flavor)
           servers.append(tmp_srv)
-#          srvs.append(servers[-1].id)
      print "Building Servers. Might take about  %s secs..." % (20*count)
      time.sleep(20*count)
      while True:
@@ -110,10 +110,11 @@ def createServers(count, name, image, flavor):
           time.sleep(2)
      return srvs
 
-creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
-pyrax.set_credential_file(creds_file)
-cs = pyrax.cloudservers
-clb = pyrax.cloud_loadbalancers
+def getFlavor(flavorname):
+     return [flv for flv in cs.flavors.list() if flv.ram == flavorname][0]
+
+def getImage(imagename):
+     return [img for img in cs.images.list() if imagename in img.name.lower()][0]
 
 parser = argparse.ArgumentParser(description='Cloud LBs Creator.')
 parser.add_argument('-a', '--algorithm', metavar='ALGORITHM', 
@@ -128,16 +129,26 @@ parser.add_argument('-f', '--flavor', metavar='SIZE', default=512, type=int,
 parser.add_argument('-I', '--image-list', action='store_true',
                     help='Prints the available Server Images.')
 parser.add_argument('-i', '--image', metavar='IMG_NAME', default='arch',
-                    help='Image to use, uses the first match. Default arch.')
+                    help='Name of Image to use(first match). Default Arch.')
+parser.add_argument('--lbname', metavar='NAME', help='Name for the LB.')
 parser.add_argument('-n', '--node-count', metavar='COUNT', default=2, type=int,
-                    help='Number of Servers behind the LB')
+                    help='Number of Servers behind the LB, default 2')
 parser.add_argument('-p', '--port', metavar='PORT', default=80, type=int,
-                    help='Port to use in the LB. Default 80')
+                    help='Port number to use in the LB. Default 80')
+parser.add_argument('--port-on-nodes', metavar='PORT', default=80, type=int,
+                    help='Port number to use on the nodes. Default 80.')
 parser.add_argument('--protocol', metavar='PROTOCOL', default='http',
-                    help='Protocol to use in the LB. Default http')
+                    help='Protocol to use in the Cloud LB. Default http')
 parser.add_argument('--protocol-list', action='store_true',
                     help='Prints the supported protocols.')
+parser.add_argument('-s', '--server-prefix', metavar='NAME',
+                     help='Prefix name for the Cloud Servers.')
 args = parser.parse_args()
+
+creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
+pyrax.set_credential_file(creds_file)
+cs = pyrax.cloudservers
+clb = pyrax.cloud_loadbalancers
 
 if args.flavor_list:
      flavorList()
@@ -171,14 +182,14 @@ if not isValidAlgorithm(args.algorithm):
      print "Invalid Algorithm: %s" % (args.algorithm)
      sys.exit(1)
 
-# Name of the cloud servers
-name = 'x' + randomStr(8)
-flavor = [flv for flv in cs.flavors.list() if flv.ram == args.flavor][0]
-image = [img for img in cs.images.list() if args.image in img.name.lower()][0]
+print "Serving your request, please wait..."
+flavor = getFlavor(args.flavor)
+image = getImage(args.image)
+sname = 'x' + args.server_prefix if args.server_prefix else randomStr(8)
+lbname = args.lbname if args.lbname else  randomStr(8)
 
-# TODO: lbname, internal/external port, algorithm
-servers = createServers(args.node_count, name, image, flavor)
-nodes = createNodes(servers, 80)
-lb = createLB("foo", args.port, args.protocol, nodes)
+servers = createServers(args.node_count, sname, image, flavor)
+nodes = createNodes(servers, args.port_on_nodes)
+lb = createLB(lbname, args.algorithm, args.port, args.protocol, nodes)
 print "LB: %s Public: %s" % (lb.name, lb.virtual_ips[0].address)
 sys.exit(0)
