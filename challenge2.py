d290db7ab7f23f9f0a0c51dea0f0defcdc3de583
tonyskapunk <tonysk8@gmx.net>
2013-03-31 18:34:04 +0000

    Remove duplicated code from challenge1.

--- challenge2.py
+++ challenge2.py
@@ -1,81 +1,11 @@
 #!/usr/bin/env python
 
 import argparse
-import os
-import pyrax
-import re
+import auth
 import sys
 import time
-
-def imageList():
-     print "Available Images:"
-     for img in cs.images.list():
-          print img.name.lower()
-
-def flavorList():
-     print "Available Flavors:"
-     for flv in cs.flavors.list():
-          print flv.ram
-
-def isValidImage(image):
-    for img in cs.images.list():
-         if str(image).lower() in img.name.lower():
-              return True
-    return False
-
-def isValidFlavor(flavor):
-    for flv in cs.flavors.list():
-         if flv.ram == flavor:
-              return True
-    return False
-
-def randomStr(length):
-    return re.sub('\W','',os.urandom(200))[:length]
-
-def getFlavor(flavorname):
-     return [flv for flv in cs.flavors.list() if flv.ram == flavorname][0]
-
-def getImage(imagename):
-     return [img for img in cs.images.list() if imagename.lower() in 
-             img.name.lower()][0]
-
-def createServers(count, name, image, flavor):
-     servers = []
-     srvs = []
-     delay = 5
-     build_time = 240 + delay * count
-     for index in range(count):
-          tmp_srv = cs.servers.create("%s%s" % (name, index), image, flavor)
-          servers.append(tmp_srv)
-     print "Building Servers. Might take about  %s secs..." % (build_time)
-     time.sleep(build_time)
-     servertime = build_time
-     while True:
-          if not servers:
-               break
-          s = servers[0]
-          del servers[0]
-          s.get()
-          if s.status == 'ACTIVE':
-               if s.networks:
-                    public = s.networks['public']
-                    if ":" in public[0]:
-                         publicv4 = public[1]
-                    else:
-                         publicv4 = public[0]
-                    print "%s: %s / %s" % (s.name, publicv4, s.adminPass)
-                    srvs.append(s)
-               else:
-                    print "Server is Active but no network info found yet."
-          elif s.status == "ERROR":
-               print "Alas! something went wrong, rebuilding %s." % s.name
-               s.delete()
-               s = cs.servers.create(s.name, image, flavor)
-               servers.append(s)
-          else:
-               servers.append(s)
-          time.sleep(delay)
-     return srvs
+from pyrax import cloudservers as cs
+from challenge1 import *
 
 if __name__ == "__main__":
      parser = argparse.ArgumentParser(description='Cloud Server Clone.')
@@ -91,10 +21,6 @@ if __name__ == "__main__":
                          help='Server Name for the *cloned* Cloud Server.')
      args = parser.parse_args()
      
-     creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
-     pyrax.set_credential_file(creds_file)
-     cs = pyrax.cloudservers
-
      if args.flavor_list:
           flavorList()
           sys.exit(0)
