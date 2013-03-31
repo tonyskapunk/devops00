46c919b388d97fffd71450d7a2675f153b98a94f
tonyskapunk <tonysk8@gmx.net>
2013-03-31 05:43:35 +0000

    Add module's name to challenge6.

--- challenge6.py
+++ challenge6.py
@@ -24,30 +24,30 @@ def isContainer(container):
         
 def randomStr(length):
     return re.sub('\W','',os.urandom(200))[:length]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Cloud File CDN Creator.')
+    parser.add_argument('-c', '--container', metavar='NAME', 
+                        help='Container name to make public via CDN.')
+    parser.add_argument('-l', '--list-containers', action='store_true',
+                        help='Prints the available containers of Cloud Files')
+    args = parser.parse_args()
 
-creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
-pyrax.set_credential_file(creds_file)
-cf = pyrax.cloudfiles
+    creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
+    pyrax.set_credential_file(creds_file)
+    cf = pyrax.cloudfiles
 
-parser = argparse.ArgumentParser(description='Cloud File CDN Creator.')
-parser.add_argument('-c', '--container', metavar='NAME', 
-                    help='Container name to make public via CDN.')
-parser.add_argument('-l', '--list-containers', action='store_true',
-                    help='Prints the available containers of Cloud Files')
-args = parser.parse_args()
 
-if args.list_containers:
-    listContainers()
+    if args.list_containers:
+        listContainers()
 
-container = randomStr(8) if not args.container else args.container
-if not isContainer(container):
-    print "Creating container: %s" % (container)
-    cont = cf.create_container(container)
-else:
-    print "Container %s already in place." % (container)
-    cont = cf.get_container(container)
+    container = randomStr(8) if not args.container else args.container
+    if not isContainer(container):
+        print "Creating container: %s" % (container)
+        cont = cf.create_container(container)
+    else:
+        print "Container %s already in place." % (container)
+        cont = cf.get_container(container)
 
-cont.make_public(ttl=600)
-print "%s\n%s\n%s\n" % (cont.cdn_uri, cont.cdn_ssl_uri, cont.cdn_streaming_uri)
-
-sys.exit(0)
+    cont.make_public(ttl=600)
+    print "%s\n%s\n%s\n" % (cont.cdn_uri, cont.cdn_ssl_uri, cont.cdn_streaming_uri)
+    sys.exit(0)
